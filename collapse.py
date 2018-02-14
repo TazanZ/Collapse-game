c38a6565e44d666d145a358a696f65a13e31c3dc
TazanZ <tim@dutch.nl>
2018-02-14 14:31:53 +0100

    game

---
+++ collapse.py
@@ -0,0 +1,59 @@
+echo "# Collapse-game" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/TazanZ/Collapse-game.git
+git push -u origin master
+
+import random
+
+cities=input("cities: ")
+farmland=input("farmland: ")
+
+gold=3
+army=3
+stability=10
+tech=10
+
+def Food(farmland,occuppied):
+  
+def Techcost(tech,gold,food):
+  
+def getGold(gold,food):
+  
+def getStability(gold,food,stability):
+
+def getarmy(gold,army,food):
+  
+def Disasters(gold,stability,food,tech):
+
+def mapgeneration(size):
+  
+class map(locations):
+    
+def enemymovement(size,types):
+  
+class Fase1(stability,army,gold,tech,food): #disasters
+  while stability<1:
+    a=Disasters()
+  for i in range(0,5-stability/3):
+    a=Disasters()
+    
+class Fase2(enemies): #moving enemies, 
+  
+class Fase3(): #actions
+
+class Fase4(): #finishing the turn, paying for stuff
+
+def combat(tech,moral): #combat, maight.
+
+while True:
+  Fase1
+  Fase2
+  print("stability:"+stability)
+  print("gold:"+gold)
+  print("food:"+food)
+  print("army:"+army)
+  print("tech:"+tech)
+  Fase3
+  Fase4
